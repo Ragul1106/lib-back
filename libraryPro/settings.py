f0211d4018d5b52cfc246a1d328d37b8fbc1f44d
Ragul116 <ragulranjith1106@gmail.com>
2025-09-10 11:14:36 +0530

    add cors

--- libraryPro/settings.py
+++ libraryPro/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -117,7 +117,8 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
-     "https://lib-fro.vercel.app/","http://localhost:5173",
+     "https://lib-fro.vercel.app",
+     "http://localhost:5173",
 ]
 
 # Static files (CSS, JavaScript, Images)
